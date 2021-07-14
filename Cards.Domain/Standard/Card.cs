fc2f2955a533fd42eb07ee78aa8b94ca8e2e46cf
Jayasree <871304@cognizant.com>
2021-07-14 01:55:37 -0500

    Checkin in codes for scenario1,2

---
+++ Cards.Domain/Standard/Card.cs
@@ -0,0 +1,9 @@
+﻿namespace Cards.Domain.Standard
+{
+    public class Card : ICard
+    {
+        public Suit Suit { get; set; }
+        public CardNumber CardNumber { get; set; }
+
+    }
+}
---
+++ DeckOfCards/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using Cards.Domain.Standard;
+
+namespace DeckOfCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var deck = new Deck();
+            var cards = deck.TakeCards(13);
+            foreach (var card in cards)
+            {
+                var newCard = card as Card;
+                Console.WriteLine(newCard.CardNumber);
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
